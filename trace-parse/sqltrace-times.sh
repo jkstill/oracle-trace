7c615063ff5b9142507506a254f56686d4e03dab
Jared Still <jkstill@gmail.com>
2025-03-29 16:29:14 -0700

    rename some vars

--- trace-parse/sqltrace-times.sh
+++ trace-parse/sqltrace-times.sh
@@ -41,18 +41,22 @@ eventRegex="^([A-Z]+).*tim=([0-9]+)"
 # this will be the first time after the start time
 # this value is in microseconds
 # for purposes of establishing timestamps, the first tim= is equivalent to the start time
-startTimeuSecs=$(grep -E 'tim=[0-9]{1,}' $traceFile | head -1 | awk -F'tim=' '{print $2}')
-prevTimeuSecs=$startTimeuSecs
-echo "First time: $startTimeuSecs"
+startTimeMicroSecs=$(grep -E 'tim=[0-9]{1,}' $traceFile | head -1 | awk -F'tim=' '{print $2}')
+prevTimeMicroSecs=$startTimeMicroSecs
+echo "First time: $startTimeMicroSecs"
 
 
-declare currTimeuSecs
-declare elapsedFromStartuSecs
-declare intervalFromPrevuSecs
+declare snmfcThreshold=1000000
+declare currTimeMicroSecs
+declare elapsedFromStartMicroSecs
+declare intervalFromPrevMicroSecs
 declare currentEpochSeconds
 declare currTimestamp
 declare computedElapsedTime=0
-declare computedElapseduSecs=0
+declare computedElapsedMicroSecs=0
+# work usecs is the total time minus the snmfc times that are skipped due to exceeding the threshold
+declare workMicroSecs=0
+declare lineType=''
 
 # now read the file getting the tim= values
 while read -r line
@@ -64,42 +68,62 @@ do
 	
 	if [[ $line =~ $waitRegex ]]; then
 		nam=${BASH_REMATCH[1]}
-		currTimeuSecs=${BASH_REMATCH[2]}
-		echo "WAIT name: $nam  time: $currTimeuSecs"
+		currTimeMicroSecs=${BASH_REMATCH[2]}
+		lineType='WAIT'
 	elif [[ $line =~ $eventRegex ]]; then
 		event=${BASH_REMATCH[1]}
-		currTimeuSecs=${BASH_REMATCH[2]}
-		echo "$event: time: $currTimeuSecs"
+		currTimeMicroSecs=${BASH_REMATCH[2]}
+		lineType='EVENT'
 	else
 		echo "Error: line did not match expected format"
 		echo "line: $line"
 		exit 1
 	fi
 
-	(( elapsedFromStartuSecs = $currTimeuSecs - $startTimeuSecs ))
-	(( intervalFromPrevuSecs = $currTimeuSecs - $prevTimeuSecs ))
+	(( elapsedFromStartMicroSecs = $currTimeMicroSecs - $startTimeMicroSecs ))
+	(( intervalFromPrevMicroSecs = $currTimeMicroSecs - $prevTimeMicroSecs ))
 
-	currentEpochSeconds=$(echo "scale=9; $startTimeEpoch + ( $intervalFromPrevuSecs / 1000000) " | bc)
-	echo "scale=9; $startTimeEpoch + ( $intervalFromPrevuSecs / 1000000) " 
+	# skip SQL*Net message from client lines if the time is greater than the threshold
+	[[ $intervalFromPrevMicroSecs -gt $snmfcThreshold ]] && [[ $line =~ 'message from client' ]] && {
+		echo "Skipping SQL*Net message from client"
+		prevTimeMicroSecs=$currTimeMicroSecs
+		continue
+	}
+
+	if [[ $lineType == 'WAIT' ]]; then
+		echo "WAIT time: $currTimeMicroSecs name: $nam"
+	else
+		echo "$event: time: $currTimeMicroSecs"
+	fi
+
+	(( workMicroSecs += intervalFromPrevMicroSecs ))
+
+	currentEpochSeconds=$(echo "scale=9; $startTimeEpoch + ( $intervalFromPrevMicroSecs / 1000000) " | bc)
+	#echo "scale=9; $startTimeEpoch + ( $intervalFromPrevMicroSecs / 1000000) " 
 	currTimestamp=$(date -d "@$currentEpochSeconds" '+%FT%T.%N')
 
 	echo "        Start Time: $startTime"
 	echo "  Start time epoch: $startTimeEpoch"
-	echo "  Start time usecs: $startTimeuSecs"
-	echo "Current time usecs: $currTimeuSecs"
-	echo " Elapsed time secs: $elapsedFromStartuSecs"
-	echo "Interval from prev: $intervalFromPrevuSecs"
+	echo "  Start time usecs: $startTimeMicroSecs"
+	echo "Current time usecs: $currTimeMicroSecs"
+	echo " Elapsed time secs: $elapsedFromStartMicroSecs"
+	echo "Interval from prev: $intervalFromPrevMicroSecs"
 	echo "Current time epoch: $currTimestamp"
 	echo
 
-	(( computedElapseduSecs += intervalFromPrevuSecs ))
+	(( computedElapsedMicroSecs += intervalFromPrevMicroSecs ))
 
-	prevTimeuSecs=$currTimeuSecs
+	prevTimeMicroSecs=$currTimeMicroSecs
 
 done < <( grep -E 'tim=[0-9]{1,}' $traceFile )
 
 echo
-echo "   Total elapsed usecs: $elapsedFromStartuSecs"
-echo "Computed elapsed usecs: $computedElapseduSecs"
+echo "   Total elapsed usecs: $elapsedFromStartMicroSecs"
+echo "Computed elapsed usecs: $computedElapsedMicroSecs"
+echo "    Work elapsed usecs: $workMicroSecs"
  
+
+
+
+
 
